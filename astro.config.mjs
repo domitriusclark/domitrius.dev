f742408aa5a90a307c64342ca3f2aeddcef04c00
Domitrius Clark <domitriusaclark@gmail.com>
2022-12-15 03:19:38 -0500

    Removed all sass definitions in favor of tailwind classes

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,24 +2,28 @@ import { defineConfig } from "astro/config";
 import { astroImageTools } from "astro-imagetools";
 
 // https://astro.build/config
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
 export default defineConfig({
   publicDir: "./public",
   outDir: "./dist",
   vite: {
-    plugins: [
-      {
-        name: "import.meta.url-transformer",
-        transform: (code, id) => {
-          if (id.endsWith(".astro"))
-            return code.replace(/import.meta.url/g, `"${id}"`);
-        },
-      },
-    ],
+    plugins: [{
+      name: "import.meta.url-transformer",
+      transform: (code, id) => {
+        if (id.endsWith(".astro")) return code.replace(/import.meta.url/g, `"${id}"`);
+      }
+    }],
     ssr: {
-      external: ["svgo"],
-    },
+      external: ["svgo"]
+    }
   },
-  experimental: { integrations: true },
-  integrations: [astroImageTools],
-  server: { port: 3001 },
+  experimental: {
+    integrations: true
+  },
+  integrations: [astroImageTools, tailwind()],
+  server: {
+    port: 3001
+  }
 });
